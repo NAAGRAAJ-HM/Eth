8ffc17615c4304cce787f329b33c6e3153772edd
root <nagaraja.hm.0001@gmail.com>
2022-01-17 09:04:49 +0100

    Update: submodule Eth

--- LATEST/Eth.cpp
+++ LATEST/Eth.cpp
@@ -5,3 +5,66 @@ class_Eth Eth;
 FUNC(void, ETH_CODE) class_Eth::InitFunction(void){
 }
 
+FUNC(void, ETH_CODE) class_Eth::GetControllerMode(void){
+}
+
+FUNC(void, ETH_CODE) class_Eth::SetControllerMode(void){
+}
+
+FUNC(void, ETH_CODE) class_Eth::GetPhysAddr(void){
+}
+
+FUNC(void, ETH_CODE) class_Eth::SetPhysAddr(void){
+}
+
+FUNC(void, ETH_CODE) class_Eth::UpdatePhysAddrFilter(void){
+}
+
+FUNC(void, ETH_CODE) class_Eth::ReadMii(void){
+}
+
+FUNC(void, ETH_CODE) class_Eth::WriteMii(void){
+}
+
+FUNC(void, ETH_CODE) class_Eth::GetCounterValues(void){
+}
+
+FUNC(void, ETH_CODE) class_Eth::GetRxStatus(void){
+}
+
+FUNC(void, ETH_CODE) class_Eth::GetTxStatus(void){
+}
+
+FUNC(void, ETH_CODE) class_Eth::GetTxErrorCounterValues(void){
+}
+
+FUNC(void, ETH_CODE) class_Eth::GetCurrentTime(void){
+}
+
+FUNC(void, ETH_CODE) class_Eth::EnableEgressTimeStamp(void){
+}
+
+FUNC(void, ETH_CODE) class_Eth::GetEgressTimeStamp(void){
+}
+
+FUNC(void, ETH_CODE) class_Eth::GetIngressTimeStamp(void){
+}
+
+FUNC(void, ETH_CODE) class_Eth::ProvideTxBuffer(void){
+}
+
+FUNC(void, ETH_CODE) class_Eth::Transit(void){
+}
+
+FUNC(void, ETH_CODE) class_Eth::Recive(void){
+}
+
+FUNC(void, ETH_CODE) class_Eth::TxConfirmation(void){
+}
+
+FUNC(void, ETH_CODE) class_Eth::GetVersionInfo(void){
+}
+
+FUNC(void, ETH_CODE) class_Eth::MainFunction(void){
+}
+
--- LATEST/Eth.h
+++ LATEST/Eth.h
@@ -5,7 +5,28 @@
 
 class class_Eth{
    public:
-      FUNC(void, ETH_CODE) InitFunction(void);
+      FUNC(void, ETH_CODE) InitFunction            (void);
+      FUNC(void, ETH_CODE) GetControllerMode       (void);
+      FUNC(void, ETH_CODE) SetControllerMode       (void);
+      FUNC(void, ETH_CODE) GetPhysAddr             (void);
+      FUNC(void, ETH_CODE) SetPhysAddr             (void);
+      FUNC(void, ETH_CODE) UpdatePhysAddrFilter    (void);
+      FUNC(void, ETH_CODE) ReadMii                 (void);
+      FUNC(void, ETH_CODE) WriteMii                (void);
+      FUNC(void, ETH_CODE) GetCounterValues        (void);
+      FUNC(void, ETH_CODE) GetRxStatus             (void);
+      FUNC(void, ETH_CODE) GetTxStatus             (void);
+      FUNC(void, ETH_CODE) GetTxErrorCounterValues (void);
+      FUNC(void, ETH_CODE) GetCurrentTime          (void);
+      FUNC(void, ETH_CODE) EnableEgressTimeStamp   (void);
+      FUNC(void, ETH_CODE) GetEgressTimeStamp      (void);
+      FUNC(void, ETH_CODE) GetIngressTimeStamp     (void);
+      FUNC(void, ETH_CODE) ProvideTxBuffer         (void);
+      FUNC(void, ETH_CODE) Transit                 (void);
+      FUNC(void, ETH_CODE) Recive                  (void);
+      FUNC(void, ETH_CODE) TxConfirmation          (void);
+      FUNC(void, ETH_CODE) GetVersionInfo          (void);
+      FUNC(void, ETH_CODE) MainFunction            (void);
 };
 
 extern class_Eth Eth;
