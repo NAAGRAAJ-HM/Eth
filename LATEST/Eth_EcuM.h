91270c48aa976e3742f1767df88f79079a7e1f6b
root <nagaraja.hm.0001@gmail.com>
2022-03-09 12:54:01 +0100

    Update: submodule Eth

--- LATEST/Eth_EcuM.h
+++ LATEST/Eth_EcuM.h
@@ -32,7 +32,7 @@
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-extern infEcuMClient *gptrinfEcuMClient_Eth;
+extern infEcuMClient* gptrinfEcuMClient_Eth;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
--- LATEST/Eth_SchM.h
+++ LATEST/Eth_SchM.h
@@ -32,7 +32,7 @@
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-extern infSchMClient *gptrinfSchMClient_Eth;
+extern infSchMClient* gptrinfSchMClient_Eth;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
