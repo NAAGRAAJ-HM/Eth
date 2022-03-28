76c6386080c73fee41b5297d3a6bf3920f28c050
root <nagaraja.hm.0001@gmail.com>
2022-03-28 12:47:11 +0200

    Update: AUTOSAR basic software micro-controller abstraction module Eth

--- LATEST/Eth.cpp
+++ LATEST/Eth.cpp
@@ -90,7 +90,28 @@ FUNC(void, ETH_CODE) module_Eth::GetVersionInfo(void){
 FUNC(void, ETH_CODE) module_Eth::MainFunction(void){
 }
 
-#include "Eth_Unused.hpp"
+class class_Eth_Unused{
+   public:
+      FUNC(void, ETH_CODE) GetControllerMode       (void);
+      FUNC(void, ETH_CODE) SetControllerMode       (void);
+      FUNC(void, ETH_CODE) GetPhysAddr             (void);
+      FUNC(void, ETH_CODE) SetPhysAddr             (void);
+      FUNC(void, ETH_CODE) UpdatePhysAddrFilter    (void);
+      FUNC(void, ETH_CODE) ReadMii                 (void);
+      FUNC(void, ETH_CODE) WriteMii                (void);
+      FUNC(void, ETH_CODE) GetCounterValues        (void);
+      FUNC(void, ETH_CODE) GetRxStatus             (void);
+      FUNC(void, ETH_CODE) GetTxStatus             (void);
+      FUNC(void, ETH_CODE) GetTxErrorCounterValues (void);
+      FUNC(void, ETH_CODE) GetCurrentTime          (void);
+      FUNC(void, ETH_CODE) EnableEgressTimeStamp   (void);
+      FUNC(void, ETH_CODE) GetEgressTimeStamp      (void);
+      FUNC(void, ETH_CODE) GetIngressTimeStamp     (void);
+      FUNC(void, ETH_CODE) ProvideTxBuffer         (void);
+      FUNC(void, ETH_CODE) Transit                 (void);
+      FUNC(void, ETH_CODE) Recive                  (void);
+      FUNC(void, ETH_CODE) TxConfirmation          (void);
+};
 
 FUNC(void, ETH_CODE) class_Eth_Unused::GetControllerMode(void){
 }
