440cee16d2e7914c82a2749150086fd919d30a42
root <nagaraja.hm.0001@gmail.com>
2022-04-07 09:50:11 +0200

    Update: AUTOSAR basic software micro-controller abstraction module Eth

--- LATEST/Eth.cpp
+++ LATEST/Eth.cpp
@@ -96,8 +96,14 @@ FUNC(void, ETH_CODE) module_Eth::InitFunction(
 #endif
       }
       else{
+         if(STD_LOW){
 // check lptrCfgModule for memory faults
-// use PBcfg_Eth as back-up configuration
+            lptrCfg = lptrCfgModule;
+         }
+         else{
+// use PBcfg_CanIf as back-up configuration
+            lptrCfg = PBcfg_CanIf;
+         }
       }
       IsInitDone = E_OK;
 #if(STD_ON == Eth_InitCheck)
