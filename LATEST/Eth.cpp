70cc29f37970871a46407b6e8c2d9f41ac4ca4ed
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-06-29 09:02:23 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module for ethernet communication protocol stack

--- LATEST/Eth.cpp
+++ LATEST/Eth.cpp
@@ -41,12 +41,8 @@ class module_Eth:
 /******************************************************************************/
 /* OBJECTS                                                                    */
 /******************************************************************************/
-            Std_TypeReturn          IsInitDone{E_NOT_OK};
-      const CfgModule_TypeAbstract* lptrCfg{(CfgModule_TypeAbstract*)NULL_PTR};
 
    public:
-      module_Eth(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
-      }
       FUNC(void, ETH_CODE) InitFunction(
          CONSTP2CONST(CfgModule_TypeAbstract, ETH_CONFIG_DATA, ETH_APPL_CONST) lptrCfgModule
       );
@@ -71,18 +67,7 @@ CONSTP2VAR(infSchMClient, ETH_VAR, ETH_CONST) gptrinfSchMClient_Eth = &Eth;
 /******************************************************************************/
 /* OBJECTS                                                                    */
 /******************************************************************************/
-VAR(module_Eth, ETH_VAR) Eth(
-   {
-         ETH_AR_RELEASE_VERSION_MAJOR
-      ,  ETH_AR_RELEASE_VERSION_MINOR
-      ,  0x00
-      ,  0xFF
-      ,  0x01
-      ,  '0'
-      ,  '1'
-      ,  '0'
-   }
-);
+VAR(module_Eth, ETH_VAR) Eth;
 
 /******************************************************************************/
 /* FUNCTIONS                                                                  */
