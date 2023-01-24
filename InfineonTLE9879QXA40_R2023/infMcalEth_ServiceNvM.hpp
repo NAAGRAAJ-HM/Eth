92237e3ff48d2ac4ce1fe74248475c9e720c5220
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:52:47 +0100

    Update: AUTOSAR basic software micro-controller abstraction driver module for ethernet communication protocol stack

---
+++ InfineonTLE9879QXA40_R2023/infMcalEth_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infMcalEth_ServiceNvM.hpp                                         */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTMCALETH_AR_RELEASE_VERSION_MAJOR                                  4
+#define CONSTMCALETH_AR_RELEASE_VERSION_MINOR                                  3
+
+#define Const_DefaultInit_ConstMcalEth                                                                           \
+   ,  {CONSTMCALETH_AR_RELEASE_VERSION_MAJOR, CONSTMCALETH_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
